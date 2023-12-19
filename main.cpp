e302329130b51f232f23ef554dca82251efd5b99
Materka11 <arekmaterka11@gmail.com>
2023-12-19 16:06:55 +0100

    Added GUI

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+
+using namespace Blackjack;
+
+[STAThreadAttribute]
+int main(array < System::String^ >^ args)
+{
+	// Enabling Windows XP visual effects before any changes are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	//Create the main window and run it 
+	Application::Run(gcnew MyForm());
+	return 0;
+}
